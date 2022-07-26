4054c0c27a6e1b63efa2d01934f875692ce04d1c
pieceofwool <fabioghide98@gmail.com>
2022-07-26 09:32:19 +0200

    6-gestione-utenti-fabio
    6-gestione-utenti-fabio

---
+++ GestioneUtenti.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Nel page load carico la griglia con TUTTI gli utenti, sia quelli abilitati che non
+        UTENTI u = new UTENTI();
+        griglia.DataSource = u.Select();
+        griglia.DataBind();
+    }
+
+    protected void btnAggiorna_Click(object sender, EventArgs e)
+    {
+        // A SECONDA DEI FILTRI SELEZIONATI CARICO NELLA GRIGLIA:
+
+        // 1. Gli utenti abilitati
+        if (checkAbilitati.Checked == true)
+        {
+            UTENTI u = new UTENTI();
+            griglia.DataSource = u.Select(true);
+            griglia.DataBind();
+        }
+
+        // 2. Gli utenti NON abilitati
+        if (checkNonAbilitati.Checked == true)
+        {
+            UTENTI u = new UTENTI();
+            griglia.DataSource = u.Select(false);
+            griglia.DataBind();
+        }
+        
+        // 3. Gli utenti abilitati e non abilitati
+        if (checkAbilitati.Checked == true & checkNonAbilitati.Checked == true)
+        {
+            UTENTI u = new UTENTI();
+            griglia.DataSource = u.Select();
+            griglia.DataBind();
+        }
+    }
+
+    protected void griglia_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        GridViewRow row = griglia.SelectedRow;
+        Session["codUtente"] = griglia.SelectedDataKey[0].ToString();
+    }
+}
---
+++ GestioneUtenti_popup.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GestioneUtenti_popup : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        UTENTI u = new UTENTI();
+
+        if (!IsPostBack)
+        {
+            int codUtente = int.Parse(Session["codUtente"].ToString()); 
+            DataTable dt = new DataTable();
+            dt = u.Select(codUtente);
+
+            txtEmail.Text = dt.Rows[0]["email"].ToString();
+            bool abilitato = Convert.ToBoolean(dt.Rows[0]["abilitato"].ToString());
+            string tipoUtente = dt.Rows[0]["tipoUtente"].ToString();
+
+            // vado a checkare automaticamente il radiobutton in base all'abilitazione dell'utente che sto cercando di modificare
+            if (abilitato == true)
+            {
+                rbtnAbilitato.Checked = true;
+            }
+
+            else
+            {
+                rbtnNonAbilitato.Checked = true;
+            }
+
+            // vado a checkare automaticamente il radiobutton in base ai permessi dell'utente che sto cercando di modificare
+            if (tipoUtente == "A")
+            {
+                rbtnAdmin.Checked = true;
+            }
+
+            if (tipoUtente == "U")
+            {
+                rbtnUser.Checked = true;
+            }
+        }
+    }
+    protected void btnModifica_Click(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(txtEmail.Text))
+        {
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Attenzione: Inserire indirizzo email')", true);
+            return;
+        }
+
+        UTENTI u = new UTENTI();
+        u.email = txtEmail.Text.Trim();
+
+        if (rbtnAbilitato.Checked == true)
+        {
+            u.abilitato = true;
+        }
+
+        if (rbtnNonAbilitato.Checked == true)
+        {
+            u.abilitato = false;
+        }
+
+        if (rbtnAdmin.Checked == true)
+        {
+            u.tipoUtente = "A";
+        }
+        
+        if (rbtnUser.Checked == true)
+        {
+            u.tipoUtente = "U";
+        }
+
+        int codUtente = int.Parse(Session["codUtente"].ToString());
+        u.codUtente = codUtente;
+
+        u.Update();
+
+        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Dati modificati correttamente.')", true);
+        return;
+    }
+}
